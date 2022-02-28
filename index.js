dc4e9eac3e7d097c298456c8f469d685a84a04a2
pranab56 <pronabhalder53@gmail.com>
2022-03-01 00:51:08 +0600

    search-phone

--- index.js
+++ index.js
@@ -14,17 +14,14 @@ console.log(phone);
   const div=document.createElement('div')
   div.classList.add('col')
   div.innerHTML=`
-
- 
- 
-            <div class="card-body shadow-lg alert-danger ">
+            <div style="width:90%; border-radius:15px;" class="card-body shadow-lg alert-danger mx-auto ">
             <img class='img-fluid' src="${phone.image}" class="card-img-top" alt="..."
- >
-              <h5 class="card-title">${phone.phone_name}</h5>
+            >
+              <h5 class="card-title mt-2">${phone.phone_name}</h5>
               <h5>${phone.brand}</h5>
+              <button class="border-0 bg-danger rounded text-white p-2">MORE DETAILS</button>
             </div>
          
-  
   `
   phoneList.appendChild(div)
  }
