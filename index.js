8e72267a4920a1cfbd780ce9a1fc0b94242ea1ab
pranab56 <pronabhalder53@gmail.com>
2022-03-01 02:45:16 +0600

    error

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ const searchPhone=()=>{
     const searchfield=document.getElementById('search-field')
     const totalsearch=searchfield.value
     searchfield.value='';
+    if(totalsearch==''){
+      const error=document.getElementById('error')
+     error.style.display='block'
+    }
+   else{
     const url=`https://openapi.programming-hero.com/api/phones?search=${totalsearch}`
     fetch(url)
     .then(res=>res.json())
     .then(data=>searchValue(data.data))
+   }
 }
 const searchValue=(phones)=>{
     const phoneList=document.getElementById('phoneList')
     phoneList.textContent='';
- for(const phone of phones){
-console.log(phone);
-  const div=document.createElement('div')
-  div.classList.add('col')
-  div.innerHTML=`
-            <div style="width:90%; border-radius:15px;" class="card-body shadow-lg alert-danger mx-auto ">
-            <img class='img-fluid' src="${phone.image}" class="card-img-top" alt="..."
-            >
-              <h5 class="card-title mt-2">${phone.phone_name}</h5>
-              <h5>${phone.brand}</h5>
-              <button onclick="phoneDetails('${phone.slug}')" class="border-0 bg-danger rounded text-white p-2">MORE DETAILS</button>
-            </div>
-         
-  `
-  phoneList.appendChild(div)
- }
+    if(phones.length==0){
+     const error=document.getElementById('error')
+     error.style.display='block'
+    }
+else{
+  for(const phone of phones){
+    console.log(phone);
+      const div=document.createElement('div')
+      div.classList.add('col')
+      div.innerHTML=`
+                <div style="width:90%; border-radius:15px;" class="card-body shadow-lg alert-danger mx-auto mb-5  ">
+                <img class='img-fluid' src="${phone.image}" class="card-img-top" alt="..."
+                >
+                  <h5 class="card-title mt-2">${phone.phone_name}</h5>
+                  <h5>${phone.brand}</h5>
+                  <button onclick="phoneDetails('${phone.slug}')" class="border-0 bg-danger rounded text-white p-2">MORE DETAILS</button>
+                </div>
+             
+      `
+      phoneList.appendChild(div)
+     }
+}
 }
 
 const phoneDetails=phoneId=>{
